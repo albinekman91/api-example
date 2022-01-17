447a3b25645da1abb0ef45ddc784b19221f74fd6
Albin Martinsson <albinmartinsson91@gmail.com>
2022-01-17 14:30:07 +0100

    add user id

--- src/index.js
+++ src/index.js
@@ -1,25 +1,42 @@
+import cors from 'cors';
 import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
 
 const app = express();
 
+app.use(cors());
+
+let users = {
+  1: {
+    id: '1',
+    username: 'Albin Martinsson',
+  },
+  2: {
+    id: '2',
+    username: 'David Öhlin',
+  },
+};
+
+const me = users[1];
+
 const schema = gql`
   type Query {
     me: User
+    user(id: ID!): User
   }
   type User {
+    id: ID!
     username: String!
-    email: String!
   }
 `;
 
 const resolvers = {
   Query: {
     me: () => {
-      return {
-        username: 'Albin Martinsson',
-        email: 'albin@kodverket.io',
-      };
+      return me;
+    },
+    user: (parent, { id }) => {
+      return users[id];
     },
   },
 };
